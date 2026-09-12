667b91fba0178dfefa7c2b9cc60c64737bbab230
asaerxd6767 <mohammedasser070@gmail.com>
2026-09-12 21:42:26 +0300

    feat: add routing for CompanyActivation and UserManagement screens

--- lib/core/navigation_manger/manger_nav.dart
+++ lib/core/navigation_manger/manger_nav.dart
@@ -1,10 +1,25 @@
+import 'package:findwork_flutter/features/admin/presentation/pages/company_activation.dart';
+import 'package:findwork_flutter/features/admin/presentation/pages/user_management.dart';
 import 'package:findwork_flutter/features/employer/presentation/pages/employer_profile/employer_profile.dart';
 import 'package:go_router/go_router.dart';
+
 GoRouter router = GoRouter(
   initialLocation: '/EmployerProfile',
   routes: [
     GoRoute(
       path: '/EmployerProfile',
       builder: (context, state) => const EmployerProfile(),
-    )
-  ]);
+    ),
+
+    // ============ ADMIN ============
+    GoRoute(
+      path: '/UserManagement',
+      builder: (context, state) => const UserManagementScreen(),
+    ),
+
+    GoRoute(
+      path: '/CompanyActivation',
+      builder: (context, state) => CompanyActivationScreen(),
+    ),
+  ],
+);
